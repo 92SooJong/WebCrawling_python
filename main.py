a60db0181151121d1592e4bf2b0fc8e14a65ef11
92SooJong <hsz3855@gmail.com>
2020-08-17 18:20:42 +0900

    섹터와 산업이 없는경우 처리

--- main.py
+++ main.py
@@ -8,7 +8,11 @@ today = datetime.today().strftime('%Y-%m-%d')
 
 
 stock_url = {}
-stock_info = {}
+stock_data = []
+
+path = today +'_usa_stock_price.csv'
+f = open(path,'w')
+
 with open("all_usa_stock_list.html", encoding="UTF8") as fp:
     soup = BeautifulSoup(fp, 'html.parser')
 
@@ -20,9 +24,10 @@ with open("all_usa_stock_list.html", encoding="UTF8") as fp:
         a_tag = tr.find_all('a', href=True)
         span_tag = tr.find_all('span')[1]
         for a in a_tag:
+            #if a['title'].startswith('D'):
             stock_url[a['title']] = a['href']
-            stock_info[a['title'].lower()] = [span_tag['data-name']]
 
+print(stock_url)
 for k, v in stock_url.items():
 
     # Setting User-Agent for crawling
@@ -42,31 +47,38 @@ for k, v in stock_url.items():
 
     company_name_with_ticker = soup.findAll('div',class_='instrumentHead')[0].find('h1').text
     price = soup.findAll('div',class_='left current-data')[0].find('span',id='last_last')
-    industry_sector = soup.findAll('div',class_='companyProfileHeader')[0].findAll('div')
 
-    industry = industry_sector[0].find('a').text
+    industry_sector = soup.findAll('div',class_='companyProfileHeader')
 
-    sector = industry_sector[1].find('a').text
+    # 섹터가 없는 화면도 있음.
+    if len(industry_sector) >0:
+        industry_sector = industry_sector[0].findAll('div')
 
+        industry = industry_sector[0].find('a').text
+        sector = industry_sector[1].find('a').text
+    else:
+        industry ='none'
+        sector ='none'
 
     company_name = company_name_with_ticker[:company_name_with_ticker.index('(')-1]
     ticker = company_name_with_ticker[company_name_with_ticker.index('(')+1:company_name_with_ticker.index(')')]
 
-
+    stock_data.append(company_name)
     # Input ticker
-    stock_info[company_name.lower()].append(ticker)
+    stock_data.append(ticker)
 
     #Input industry
-    stock_info[company_name.lower()].append('industry')
-    stock_info[company_name.lower()].append(industry)
+    stock_data.append('industry')
+    stock_data.append(industry)
+
 
     # Input Sector
-    stock_info[company_name.lower()].append('sector')
-    stock_info[company_name.lower()].append(sector)
+    stock_data.append('sector')
+    stock_data.append(sector)
 
     # Input price
-    stock_info[company_name.lower()].append('price')
-    stock_info[company_name.lower()].append(price.text)
+    stock_data.append('price')
+    stock_data.append(price.text)
 
 
 
@@ -79,26 +91,20 @@ for k, v in stock_url.items():
     for i,s in enumerate(span_tag):
         info = s.text
 
-        stock_info[company_name.lower()].append(info)
+        stock_data.append(info)
 
-    sec = int(random.uniform(1,5))
-    time.sleep(sec)
-    print(stock_info[company_name.lower()])
+    #sec = int(random.uniform(1,5))
+    #time.sleep(sec)
+    print(stock_data)
 
-
-
-# write csv
-path = today +'_usa_stock_price.csv'
-f = open(path,'w')
-for k,v in stock_info.items():
-    company_nm_ticker = v[0] +'|' +v[1]
-
-    for i in range(2,len(v)):
-        row =''
-        if i%2 == 0 :
-            row += company_nm_ticker +'|' +v[i] +'|' + v[i+1].replace(',','') +'\n'
+    # write csv
+    company_nm_ticker = stock_data[0] + '|' + stock_data[1]
+    for i in range(2, len(stock_data)):
+        row = ''
+        if i % 2 == 0:
+            row += company_nm_ticker + '|' + stock_data[i] + '|' + stock_data[i + 1].replace(',', '') + '\n'
             f.write(row)
-
+    stock_data.clear()
 
 f.close()
 
